091857995690632894acd0bd732a3147155b85d2
Dragan <github@registratura.org>
2014-02-01 09:31:18 +0100

    Backup

--- Source/Code/Collections/ICodeSet/ICodeSetService.cs
+++ Source/Code/Collections/ICodeSet/ICodeSetService.cs
@@ -247,7 +247,7 @@ namespace DD.Collections
         [Pure] public static int CompareTo (this ICodeSet self, ICodeSet that) {
             
             if (ICodeSetService.QuickSetEquals (self, that) == true) { return 0; }
-            Contract.Assume (!(self.IsNull() || that.IsNull()));
+            Contract.Assume (!(self.IsNull() && that.IsNull()));
 
             if (self.IsNull()) { return -1; }
             if (that.IsNull()) { return 1; }
