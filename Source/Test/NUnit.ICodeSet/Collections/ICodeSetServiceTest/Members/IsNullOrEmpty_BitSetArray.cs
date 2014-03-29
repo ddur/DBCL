585deb30846e90d1bca47474d3a93c66b0802e52
Dragan <github@registratura.org>
2014-03-29 20:37:18 +0100

    ICodeSetService.Test.IsNullOrEmpty

---
+++ Source/Test/NUnit.ICodeSet/Collections/ICodeSetServiceTest/Members/IsNullOrEmpty_BitSetArray.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------
+// <copyright file="https://github.com/ddur/DBCL/blob/master/LICENSE" company="DD">
+// Copyright © 2013-2014 Dragan Duric. All Rights Reserved.
+// </copyright>
+// --------------------------------------------------------------------------------
+
+using System;
+using NUnit.Framework;
+
+namespace DD.Collections.ICodeSetServiceTest.Members
+{
+	[TestFixture]
+	public class IsNullOrEmpty_BitSetArray
+	{
+		[Test]
+		public void IsTrue()
+		{
+			Assert.True (((BitSetArray)null).IsNullOrEmpty());
+			Assert.True ((new BitSetArray()).IsNullOrEmpty());
+			Assert.True ((new BitSetArray(100)).IsNullOrEmpty());
+		}
+
+		[Test]
+		public void IsFalse()
+		{
+			Assert.False ((new BitSetArray(){0}).IsNullOrEmpty());
+			Assert.False ((new BitSetArray(100){1,2,3}).IsNullOrEmpty());
+			Assert.False ((new BitSetArray(100, true)).IsNullOrEmpty());
+		}
+	}
+}
---
+++ Source/Test/NUnit.ICodeSet/Collections/ICodeSetServiceTest/Members/IsNullOrEmpty_ICodeSet.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// <copyright file="https://github.com/ddur/DBCL/blob/master/LICENSE" company="DD">
+// Copyright © 2013-2014 Dragan Duric. All Rights Reserved.
+// </copyright>
+// --------------------------------------------------------------------------------
+
+using System;
+using NUnit.Framework;
+
+namespace DD.Collections.ICodeSetServiceTest.Members
+{
+	[TestFixture]
+	public class IsNullOrEmpty_ICodeSet
+	{
+		[Test]
+		public void IsTrue()
+		{
+			Assert.True (((ICodeSet)null).IsNullOrEmpty());
+			Assert.True (((CodeSetBits)null).IsNullOrEmpty());
+			Assert.True (((CodeSetNull)null).IsNullOrEmpty());
+			Assert.True (((CodeSetPair)null).IsNullOrEmpty());
+			Assert.True (((CodeSetList)null).IsNullOrEmpty());
+			Assert.True (((CodeSetFull)null).IsNullOrEmpty());
+			Assert.True (((CodeSetDiff)null).IsNullOrEmpty());
+			Assert.True (((CodeSetPage)null).IsNullOrEmpty());
+			Assert.True (((CodeSetWide)null).IsNullOrEmpty());
+
+			Assert.True ((CodeSetNull.Singleton).IsNullOrEmpty());
+			Assert.True ((new CodeSetBits()).IsNullOrEmpty());
+		}
+
+		[Test]
+		public void IsFalse() {
+			Assert.False ((new CodeSetBits(new Code[] {0})).IsNullOrEmpty());
+			Assert.False ((new Code(9)).IsNullOrEmpty());
+
+			Assert.False ((new CodeSetBits(new Code[] {10,11})).IsNullOrEmpty());
+			Assert.False ((new CodeSetPair(10,11)).IsNullOrEmpty());
+		}
+	}
+}
