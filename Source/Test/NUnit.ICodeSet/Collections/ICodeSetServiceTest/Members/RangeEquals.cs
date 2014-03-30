e86c0cab12b8d73d9aa74061c539d68466e914b2
Dragan <github@registratura.org>
2014-03-30 19:48:12 +0200

    ICodeSetService.Test.RangeEqual

--- Source/Test/NUnit.ICodeSet/Collections/ICodeSetServiceTest/Members/RangeEquals.cs
+++ Source/Test/NUnit.ICodeSet/Collections/ICodeSetServiceTest/Members/RangeEquals.cs
@@ -13,9 +13,100 @@ namespace DD.Collections.ICodeSetServiceTest.Members
 	public class RangeEquals
 	{
 		[Test]
-		public void TestMethod()
+		public void Null()
 		{
-			// TODO: Add your test.
+			ICodeSet a = null;
+			ICodeSet b = null;
+			
+			Assert.IsTrue (a.RangeEquals(b));
+			Assert.IsTrue (b.RangeEquals(a));
+
+			b = new Code(7);
+
+			Assert.IsFalse (a.RangeEquals(b));
+			Assert.IsFalse (b.RangeEquals(a));
+		}
+
+		[Test]
+		public void Empty()
+		{
+			ICodeSet a = new CodeSetBits();
+			ICodeSet b = CodeSetNull.Singleton;;
+			
+			Assert.IsTrue (a.RangeEquals(b));
+			Assert.IsTrue (b.RangeEquals(a));
+
+			b = new CodeSetPair(7,12);
+
+			Assert.IsFalse (a.RangeEquals(b));
+			Assert.IsFalse (b.RangeEquals(a));
+		}
+
+		[Test]
+		public void NullOrEmpty()
+		{
+			ICodeSet a = null;
+			ICodeSet b = CodeSetNull.Singleton;;
+			
+			Assert.IsTrue (a.RangeEquals(b));
+			Assert.IsTrue (b.RangeEquals(a));
+		}
+
+		[Test]
+		public void ReferenceEqual()
+		{
+			ICodeSet a = null;
+			
+			Assert.IsTrue (a.RangeEquals(a));
+
+			a = new CodeSetList(1,3,7,8,9);
+
+			Assert.IsTrue (a.RangeEquals(a));
+		}
+
+		[Test]
+		public void SetEqual()
+		{
+			ICodeSet a = new CodeSetBits(6,9,28);
+			ICodeSet b = new CodeSetList(6,9,28);
+			
+			Assert.IsTrue (a.RangeEquals(b));
+			Assert.IsTrue (b.RangeEquals(a));
+		}
+
+		[Test]
+		public void RangeEqual()
+		{
+			ICodeSet a = new CodeSetList(6,9,28);
+			ICodeSet b = new CodeSetPair(6,28);
+			
+			Assert.IsTrue (a.RangeEquals(b));
+			Assert.IsTrue (b.RangeEquals(a));
+		}
+
+		[Test]
+		public void NotRangeEqual()
+		{
+			ICodeSet a = new CodeSetList(6,9,28);
+			ICodeSet b = new CodeSetPair(6,27);
+			
+			Assert.IsFalse (a.RangeEquals(b));
+			Assert.IsFalse (b.RangeEquals(a));
+
+			b = new CodeSetPair (9,28);
+			
+			Assert.IsFalse (a.RangeEquals(b));
+			Assert.IsFalse (b.RangeEquals(a));
+
+			b = new CodeSetPair (28,29);
+			
+			Assert.IsFalse (a.RangeEquals(b));
+			Assert.IsFalse (b.RangeEquals(a));
+
+			b = new Code (6);
+			
+			Assert.IsFalse (a.RangeEquals(b));
+			Assert.IsFalse (b.RangeEquals(a));
 		}
 	}
 }
