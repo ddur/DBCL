50b9a4fc695d14d050dabf60e8c3b8b868b10072
ddur <github@registratura.org>
2015-12-20 18:41:11 +0100

    Add codecov token, improve test-coverage.bat

--- test-coverage.bat
+++ test-coverage.bat
@@ -7,43 +7,56 @@
 :test-coverage
 @if exist .\artifacts\. (del /Q .\artifacts\*) else (md .\artifacts)
 
-@Rem OpenCover release build @"E:\GitHub\opencover\main\bin\Debug\OpenCover.Console.exe" -register:user -mergebyhash ^
-@Rem OpenCover MSI installed @"C:\Program Files (x86)\OpenCover\OpenCover.Console.exe" -register:user -mergebyhash ^
-@Rem OpenCover nuget package @".\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -register:user -mergebyhash ^
+@Rem OpenCover release build
+@set OpenCoverReleaseBuild=E:\GitHub\opencover\main\bin\Debug\OpenCover.Console.exe
 
-@".\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -register:user -mergebyhash ^
+@Rem OpenCover MSI installed
+@set OpenCoverMsiInstalled=C:\Program Files (x86)\OpenCover\OpenCover.Console.exe
+
+@Rem OpenCover nuget package
+@set OpenCoverNugetPackage=.\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe
+
+@Rem OpenCover options
+@set OpenCoverOptions=-register:user -mergebyhash -hideskipped:All
+
+@Rem OpenCover command
+@set OpenCoverCommand=%OpenCoverNugetPackage% %OpenCoverOptions%
+
+@rem %OpenCoverCommand% %OpenCoverOptions%
+
+@%OpenCoverCommand% ^
 -output:".\artifacts\OpenCover.BitSetArray.xml" ^
--filter:"-[*]DD.Collections.BitSetArray.*Test* +[*]DD.Collections.BitSetArray*" ^
+-filter:"-[*]DD.Collections.BitSetArrayTest* +[*]DD.Collections.BitSetArray*" ^
 -target:".\packages\NUnit.Runners.Net4.2.6.4\tools\nunit-console-x86.exe" ^
 -targetdir:".\Source\Test\NUnit.BitSetArray\bin\Debug" ^
--targetargs:"\"NUnit.BitSetArray.dll\" /labels /xml=\"BitSetArray.TestResult.xml\" "
+-targetargs:"\"NUnit.BitSetArray.dll\" /labels /xml=\"..\..\BitSetArray.TestResult.xml\" "
 @echo -------------------------------------
 @echo.
 @echo.
-@".\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -register:user -mergebyhash ^
+@%OpenCoverCommand% ^
 -output:".\artifacts\OpenCover.ICodeSet.xml" ^
 -filter:"-[*]DD.Collections.ICodeSet.*Test* +[*]DD.Collections.ICodeSet* +[*]DD.Text*" ^
 -target:".\packages\NUnit.Runners.Net4.2.6.4\tools\nunit-console-x86.exe" ^
 -targetdir:".\Source\Test\NUnit.ICodeSet\bin\Debug" ^
--targetargs:"\"NUnit.ICodeSet.dll\" /labels /xml=\"ICodeSet.TestResult.xml\" "
+-targetargs:"\"NUnit.ICodeSet.dll\" /labels /xml=\"..\..\ICodeSet.TestResult.xml\" "
 @echo -------------------------------------
 @echo.
 @echo.
-@".\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -register:user -mergebyhash ^
+@%OpenCoverCommand% ^
 -output:".\artifacts\OpenCover.Extensions.xml" ^
 -filter:"+[*]DD.Extends*" ^
 -target:".\packages\NUnit.Runners.Net4.2.6.4\tools\nunit-console-x86.exe" ^
 -targetdir:".\Source\Test\NUnit.Extensions\bin\Debug" ^
--targetargs:"\"NUnit.Extensions.dll\" /labels /xml=\"Extensions.TestResult.xml\" "
+-targetargs:"\"NUnit.Extensions.dll\" /labels /xml=\"..\..\TestResult.Extensions.xml\" "
 @echo -------------------------------------
 @echo.
 @echo.
-@".\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -register:user -mergebyhash ^
+@%OpenCoverCommand% ^
 -output:".\artifacts\OpenCover.Diagnostics.xml" ^
--filter:"+[*]DD.Diagnostics*" ^
+-filter:"-[*]DD.Diagnostics.SuccessTest* +[*]DD.Diagnostics*" ^
 -target:".\packages\NUnit.Runners.Net4.2.6.4\tools\nunit-console-x86.exe" ^
 -targetdir:".\Source\Test\NUnit.Diagnostics\bin\Debug" ^
--targetargs:"\"NUnit.Diagnostics.dll\" /labels /xml=\"Diagnostics.TestResult.xml\" "
+-targetargs:"\"NUnit.Diagnostics.dll\" /labels /xml=\"..\..\TestResult.xml\" "
 @echo -------------------------------------
 @echo.
 @echo.
