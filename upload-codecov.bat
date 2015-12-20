94734865699687838d3badb6d9ae37fbae1e674e
ddur <github@registratura.org>
2015-12-20 19:35:19 +0100

    Codecovcrap options

--- upload-codecov.bat
+++ upload-codecov.bat
@@ -1,5 +1,5 @@
 @if not "%appveyor%" == "true" goto end
 @set path=C:\\Python34;C:\\Python34\\Scripts;%PATH%
 pip install codecov
-codecov -t "164a4e94-5b54-4162-b500-b290a69c8307" -f ".\artifacts\OpenCover.BitSetArray.xml"
+codecov -X gcov -t "164a4e94-5b54-4162-b500-b290a69c8307" -f ".\artifacts\OpenCover.BitSetArray.xml"
 :end
