42e24ffb4f9d06bd3ca1cabf7a88718938de205a
ddur <github@registratura.org>
2015-12-20 15:12:53 +0100

    AppVeyor build 4

--- upload-coverage.ps1
+++ upload-coverage.ps1
@@ -1,6 +1,5 @@
-if ($env:appveyor == "true") {
+if (Test-Path env:appveyor) {
 .\packages\coveralls.io.1.3.4\tools\coveralls.net.exe --opencover ".\artifacts\OpenCover.*.xml" --repo-token "3HvPffZf6UKHBmBX3kZG0NSV50g3yyej5"
 (New-Object System.Net.WebClient).DownloadFile("https://codecov.io/bash", ".\CodecovUploader.sh")
 .\CodecovUploader.sh -X gcov -f ".\artifacts\OpenCover.*.xml"
 }
-
