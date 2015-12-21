bfb5b8d96ab24567facf4f3ccf205ecbdbd5e2db
ddur <github@registratura.org>
2015-12-21 11:45:02 +0100

    Update upload-codecov.bat

--- upload-codecov.bat
+++ upload-codecov.bat
@@ -1,5 +1,5 @@
 @if not "%appveyor%" == "true" goto end
-@set path=C:\\Python34;C:\\Python34\\Scripts;%PATH%
-pip install codecov
-codecov -X gcov -t "164a4e94-5b54-4162-b500-b290a69c8307" -f ".\artifacts\OpenCover.BitSetArray.xml"
+@rem set path=C:\\Python34;C:\\Python34\\Scripts;%PATH%
+@rem pip install codecov
+@rem codecov -X gcov -t "164a4e94-5b54-4162-b500-b290a69c8307" -f ".\artifacts\OpenCover.BitSetArray.xml"
 :end
