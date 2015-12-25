a332b442ef7ce1dbc71b4ae02fd1bf93fc52f172
ddur <github@registratura.org>
2015-12-25 14:14:53 +0100

    yaml 8

--- zip-reports.bat
+++ zip-reports.bat
@@ -1,5 +1,5 @@
 @echo off
-@if "%appveyor%"=="True" appveyor PushArtifact "%dbcl_artifacts_folder%\OpenCover.*.xml"
+@if "%appveyor%"=="True" appveyor PushArtifact "%dbcl_artifacts_folder%\OpenCover.Extensions_b.xml"
 @%dbcl_zip_console% a "%dbcl_artifacts_folder%\CodeCoverage.7z" "%dbcl_artifacts_folder%\*"
 @echo -------------------------------------
 @echo.
