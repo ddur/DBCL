0367dd45235138cfdac0dfde90c5ad344051a6a7
ddur <github@registratura.org>
2015-12-26 13:11:26 +0100

    upload test results? 1

--- upload-testresults.ps1
+++ upload-testresults.ps1
@@ -1,2 +1,2 @@
 $wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", "($env:APPVEYOR_BUILD_FOLDER)\reports\NUnit*.xml")
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/($env:APPVEYOR_JOB_ID)", "($env:APPVEYOR_BUILD_FOLDER)\reports\NUnit*.xml")
