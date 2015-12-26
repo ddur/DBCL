8f06365dd1a6f3b3ad22bd989b729ada61724062
ddur <github@registratura.org>
2015-12-26 21:42:31 +0100

    codecov, no batch @

--- upload-codecov.bat
+++ upload-codecov.bat
@@ -1,8 +1,8 @@
-@if not "%appveyor%" == "True" goto end
-@"SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%"
-@pip install codecov
-@codecov -f "c:\projects\dbcl\reports\OpenCover.BitSetArray.xml"
-@codecov -f "c:\projects\dbcl\reports\OpenCover.ICodeSet.xml"
-@codecov -f "c:\projects\dbcl\reports\OpenCover.Extensions.xml"
-@codecov -f "c:\projects\dbcl\reports\OpenCover.Diagnostics.xml"
+if not "%appveyor%" == "True" goto end
+"SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%"
+pip install codecov
+codecov -f "c:\projects\dbcl\reports\OpenCover.BitSetArray.xml"
+codecov -f "c:\projects\dbcl\reports\OpenCover.ICodeSet.xml"
+codecov -f "c:\projects\dbcl\reports\OpenCover.Extensions.xml"
+codecov -f "c:\projects\dbcl\reports\OpenCover.Diagnostics.xml"
 :end
