c71f8e4ee47b43ccb0c747716c86f02334aa3767
ddur <github@registratura.org>
2015-12-26 22:28:53 +0100

    again

--- upload-codecov.bat
+++ upload-codecov.bat
@@ -1,6 +1,3 @@
 "SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%"
 pip install codecov
-codecov -f "c:\projects\dbcl\reports\OpenCover.BitSetArray.xml"
-codecov -f "c:\projects\dbcl\reports\OpenCover.ICodeSet.xml"
 codecov -f "c:\projects\dbcl\reports\OpenCover.Extensions.xml"
-codecov -f "c:\projects\dbcl\reports\OpenCover.Diagnostics.xml"
