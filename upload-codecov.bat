eb86ba6a02d38a186e8f8d6efb786d86d2a58da3
ddur <github@registratura.org>
2016-01-06 09:54:23 +0100

    Codecov quiet upload?

---
+++ upload-codecov.bat
@@ -0,0 +1,5 @@
+pip install codecov
+codecov -X gcov -f "c:\projects\dbcl\reports\OpenCover.BitSetArray.xml"
+codecov -X gcov -f "c:\projects\dbcl\reports\OpenCover.ICodeSet.xml"
+codecov -X gcov -f "c:\projects\dbcl\reports\OpenCover.Extensions.xml"
+codecov -X gcov -f "c:\projects\dbcl\reports\OpenCover.Diagnostics.xml"
