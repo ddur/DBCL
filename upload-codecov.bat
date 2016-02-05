cbee47fef7cb68e14b14bd40578ea0a9a0692724
ddur <github@registratura.org>
2016-02-05 01:13:32 +0100

    Upload new test

--- upload-codecov.bat
+++ upload-codecov.bat
@@ -2,4 +2,4 @@ pip install codecov
 codecov -X gcov -f "c:\projects\dbcl\reports\OpenCover.BitSetArray.xml"
 codecov -X gcov -f "c:\projects\dbcl\reports\OpenCover.ICodeSet.xml"
 codecov -X gcov -f "c:\projects\dbcl\reports\OpenCover.Extensions.xml"
-codecov -X gcov -f "c:\projects\dbcl\reports\OpenCover.Diagnostics.xml"
+codecov -X gcov -f "c:\projects\dbcl\reports\OpenCover.Text.xml"
