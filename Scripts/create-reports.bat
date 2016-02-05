d8667fcbb4d4b3ee0ba1c4b7b812ef47575b8606
ddur <github@registratura.org>
2016-02-05 19:58:10 +0100

    AppVeyor Artifacts

--- Scripts/create-reports.bat
+++ Scripts/create-reports.bat
@@ -1,4 +1,5 @@
 @%dbcl_report_generator_console% %dbcl_report_generator_options%
+@%dbcl_zip_console% a "%dbcl_artifacts_folder%\ReportGenerator.zip" "%dbcl_artifacts_folder%\*.js"  "%dbcl_artifacts_folder%\*.htm"
 @echo -------------------------------------
 @echo.
 @echo.
